0717dcc13f6b3ef36e074f6d0759c302b76df25f
Shofikul2024 <islamshofi942@gmail.com>
2025-02-23 22:48:55 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  64 page

---
+++ page-49.js
@@ -0,0 +1,9 @@
+const country ="bangladesh"
+const passed =false
+const price =555 ;
+
+
+console.log( typeof country)
+console.log(typeof passed)
+console.log(typeof price)
+ 
---
+++ page-64.js
@@ -0,0 +1,5 @@
+const first =12.13568 ;
+const second =31.22564 ;
+const total =first+second ;
+const totalNumber =parseFloat(total.toFixed(2)) ;
+console.log(totalNumber)
