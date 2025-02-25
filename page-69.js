44cee79e7b0efe83e41d8598e46f81a81e0c0ecb
Shofikul2024 <islamshofi942@gmail.com>
2025-02-25 11:38:41 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  69 page

--- page-69.js
+++ page-69.js
@@ -10,7 +10,12 @@
 
 
 //   correction 
-const peyaraPrice = 40 ;
-const applePrice = 250 ;
-console.log(peyaraPrice<applePrice)
 
+
+// const peyaraPrice = 40 ;
+// const applePrice = 250 ;
+// console.log(peyaraPrice<applePrice)
+
+const myName = "Rahim";
+const friendName = "karim";
+console.log(myName==friendName)
