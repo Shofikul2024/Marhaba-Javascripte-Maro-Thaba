5ebe72b2953abfe5e7039f63f01bb7aebf5b96a5
Shofikul2024 <islamshofi942@gmail.com>
2025-02-27 14:14:43 +0600

    Marhaba-Javascripte-Maro-Thaba ,added    75   page

--- page-75.js
+++ page-75.js
@@ -14,14 +14,26 @@
 //     console.log("i am small . i do not  bite ");
 // }
 
-if (13<10) {
+// if (13<10) {
 
-    console.log(" Smaller but stronger ");
-}
+//     console.log(" Smaller but stronger ");
+// }
 
-console.log(" jehetu condition false  tai   , condition ar majer  part toko output   dekhabe na ")
+// console.log(" jehetu condition false  tai   , condition ar majer  part toko output   dekhabe na ")
  
 
 
 
+
+
+
+
+
+const biriyaniPrice =500 ;
+if (biriyaniPrice<300)  {
+console.log (" mama gige me some biryani ")
+
+}
+
+console.log("jehetu condition vul tai if ar majer   part tokop dekhabe na ")
 
