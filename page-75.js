c4bea5da9ed1ebeb3a8f1f9504195bd9e9f9c60d
Shofikul2024 <islamshofi942@gmail.com>
2025-02-27 14:06:39 +0600

    Marhaba-Javascripte-Maro-Thaba ,added    75   page

--- page-75.js
+++ page-75.js
@@ -1,6 +1,22 @@
-if (3<10) {
+// if (3<10) {
+
+//     console.log("i am small . i do not  bite ");
+// }
+
+
+
+
+
+
+
+if (13<10) {
 
     console.log("i am small . i do not  bite ");
 }
+
+
+console.log(" this condition is false ");
+
+
 
 
