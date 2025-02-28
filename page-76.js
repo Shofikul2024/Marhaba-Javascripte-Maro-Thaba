9174614b6d1295a2bc54ab2aa8f05f88fdcb4f06
Shofikul2024 <islamshofi942@gmail.com>
2025-02-28 09:45:07 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  76   page

--- page-76.js
+++ page-76.js
@@ -1,3 +1,20 @@
+// const rainingOutsode=true ;
+// if (rainingOutsode==true)  {
+// console.log (" you and me under the tre")
+
+// }
+
+// else{
+
+//     console.log("  no rain , no romance ")
+// }
+
+
+
+
+
+
+//  aita ke output a  dekhale      you and me under the tre 
 const rainingOutsode=true ;
 if (rainingOutsode==true)  {
 console.log (" you and me under the tre")
