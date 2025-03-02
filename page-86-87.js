5c0c9ee114a409657772e84ed5f4a52a22495834
Shofikul2024 <islamshofi942@gmail.com>
2025-03-02 07:12:56 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  82 page

---
+++ page-86-87.js
@@ -0,0 +1,44 @@
+// let isLeader =true ;
+// if (isLeader===true){
+//     console.log(" government money is my money ")
+// }
+
+
+
+
+
+
+
+//  87 page ar part 
+  
+
+// let isLeader =false ;
+// if(isLeader){
+//     console.log(" government money is my money ") ;
+// }
+// console.log( "jeheto condisition vul tai   vul bartati  dekhaben ")
+
+
+// const isPassed =true;
+// if (isPassed== false ){
+
+//     console.log("ajke  khawya dawya bondho ")
+// }
+ 
+
+  
+// console.log ("ajke amr valo gift ace ")
+
+
+//  aita ke aber  short kore  likha jay  .
+
+
+
+
+const isPassed =true;
+if (!isPassed){
+
+    console.log("ajke amer  khawya dawya bondho")
+}
+
+console.log("ajke amer  valo gift ace ")
