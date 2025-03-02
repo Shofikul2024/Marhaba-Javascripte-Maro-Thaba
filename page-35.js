8c774aa8493fd08e8fad65202214726cf942e2aa
Shofikul2024 <islamshofi942@gmail.com>
2025-03-02 09:05:38 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  82 page

--- page-35.js
+++ page-35.js
@@ -5,5 +5,24 @@
 // const brotherSister= 4 ;
 // console.log(brotherSister)
 
-const student = 51 ;
-console.log( student)
+// const student = 51 ;
+// console.log( student)
+
+// const examNumber = 3.89 ;
+// console.log( examNumber)
+
+
+
+
+
+// const houseNumber = 3;
+// console.log( houseNumber)
+
+// const houseNumber = 3;
+// console.log( houseNumber)
+
+// const studyTime = 10;
+// console.log( studyTime)
+
+const digitNumber = 11;
+console.log(digitNumber)
