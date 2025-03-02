a4b3ec7c72753149c5ea81f95ded0b1a39033e6a
Shofikul2024 <islamshofi942@gmail.com>
2025-03-02 13:58:22 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  82 page

--- page-52.js
+++ page-52.js
@@ -1,3 +1,15 @@
-let  balance = 500 ;
-  balance = 400 ;
-console.log(balance)
+// let  balance = 500 ;
+//   balance = 400 ;
+// console.log(balance)
+
+
+
+//  akhan theke ja bujte parlam je let diye declear  and update korte   chile korle sorbosesh  
+// variable ar   man jeita bosabo oita output  a dekhabe 
+
+let  name  = "kala Mia " ;
+  
+name = "lal mia " ;
+console.log(name)
+
+
