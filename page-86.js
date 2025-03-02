cd7f4fabe865a2115dbc29c51ec24855804c5d56
Shofikul2024 <islamshofi942@gmail.com>
2025-03-02 06:59:17 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  82 page

--- page-86.js
+++ page-86.js
@@ -1,4 +1,13 @@
 let isLeader =true ;
-if (isLeader===true){
-    console.log(" government money is my money ")
+// if (isLeader===true){
+//     console.log(" government money is my money ")
+// }
+
+
+
+//  87 page ar part 
+// let isLeader =true ;
+
+if(isLeader){
+    console.log(" government money is my money ") ;
 }
