025efb3a9c153c296cc9032ec33e4f7c2500f3b8
Shofikul2024 <islamshofi942@gmail.com>
2025-03-03 05:05:50 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  82 page

---
+++ page-88.js
@@ -0,0 +1,30 @@
+// const  age= 17 ;
+
+// if (age >18 ){
+
+
+// console.log( " you can get vot hot ")
+
+// }
+
+// else { 
+    
+//     console.log(" you can not gety vot ")
+
+// }
+
+
+
+const age = 20 ;
+if (age >18 )
+{ 
+    
+    console.log( " ami vul  dite parbo , karon amr boyos akhon 18 theke beshio to tai ")
+}
+
+else {
+
+
+ console.log ( "  condisition  puron   na korle vut dite parbana ")
+
+}
