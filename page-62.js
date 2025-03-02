725abc5309910ce7db86b3d237c35fff5620d727
Shofikul2024 <islamshofi942@gmail.com>
2025-03-03 04:40:53 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  82 page

--- page-62.js
+++ page-62.js
@@ -1,3 +1,8 @@
-const  fatherAge=' 50'
-const  fatherAgeNumber=parseInt(fatherAge)
+// const  fatherAge=' 50'
+// const  fatherAgeNumber=parseInt(fatherAge)
+// console.log(fatherAgeNumber)
+
+
+const  fatherAge=' 50.12'
+const  fatherAgeNumber=parseFloat(fatherAge)
 console.log(fatherAgeNumber)
