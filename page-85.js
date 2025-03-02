33ddd4d6b900a359bdaafff2500bb66594487b58
Shofikul2024 <islamshofi942@gmail.com>
2025-03-02 06:36:14 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  85 page

---
+++ page-85.js
@@ -0,0 +1,20 @@
+const money = 300 ;
+const  popcornPrice =40 ;
+if(money >=300 )
+{
+
+console.log("go to watch movie") ;
+if (popcornPrice<=50) {
+console.log("bying popcorn")
+
+}
+else
+{
+    console.log("popcorn is expensive ")
+}
+
+}
+else{
+    
+    console.log("home alone ")
+}
