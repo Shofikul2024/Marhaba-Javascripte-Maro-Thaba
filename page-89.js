208edc2fec72a9ce097d855832d65b353ef59cb9
Shofikul2024 <islamshofi942@gmail.com>
2025-03-03 06:19:17 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  85 page

--- page-89.js
+++ page-89.js
@@ -1,3 +1,6 @@
+//  general   vabe ai rules follow kore solution kora  hay .  but short   vabe korter hole ternary operator use   korte  hobe 
+//  ai kaj ta kei ternary  operatr diye easily kora jay and  sohoj vabe 
+
 
 // const age = 18;
 // if (age >=18 )
@@ -19,8 +22,8 @@
 
 
 
-const age = 18 ;
-age  >= 18 ?console.log("vote dite parbe , Eligible ") :console.log("vote dite parbe na ,Not Eligible ")
+// const age = 18 ;
+// age  >= 18 ?console.log("vote dite parbe , Eligible ") :console.log("vote dite parbe na ,Not Eligible ")
 
 
 
@@ -30,4 +33,15 @@ age  >= 18 ?console.log("vote dite parbe , Eligible ") :console.log("vote dite p
 
 // const age =20 ;
  
-// age>=18 ?console.log(" you can get vote , Eligible ") : console.log(" Vote dite parva na , noNot Eligible 
+// age>=18 ?console.log(" Eligible ") : console.log("  Not Eligible )
+
+
+// const age = 20 ;
+// age>= 18 ? console.log("Eligible ") :console.log("Not Eligible ")
+
+
+
+//  ternary operator use kore short vabe  solution kora holo 
+
+const age = 18
+age >= 18 ? console.log(" Eligible , you can get the vut ") : console.log("you can not get vute . not Eligible ")
