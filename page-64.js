903c36c23d49459186c3c91407ff4394d98c04ad
Shofikul2024 <islamshofi942@gmail.com>
2025-03-03 10:41:22 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  85 page

--- page-64.js
+++ page-64.js
@@ -1,13 +1,14 @@
-const first =12.13568 ;
-const second =31.22564 ;
-const total =first+second ;
-const totalNumber =parseFloat(total.toFixed(5)) ;
-console.log(totalNumber)
-
-
+// 1 nomber aita ke console  koa hole output a string value asbe 
 
 // const first =12.13568 ;
 // const second =31.22564 ;
 // const total =first+second ;
-// const totalNumber =parseFloat(total.toFixed(2)) ;
+// const totalNumber =(total.toFixed(5)) ;
 // console.log(totalNumber)
+
+// 2. NUMBER  aita ke console  koa hole output a number ee asbe  value asbe 
+const first =12.13568 ;
+const second =31.22564 ;
+const total =first+second ;
+const totalNumber =parseFloat(total.toFixed(2)) ;
+console.log(totalNumber)
---
+++ page-95.js
@@ -0,0 +1,34 @@
+/*  akhane arrey ar koita  upadan ace oita ke  akta size namok variable 
+ ar maje rekhe diye  oi size ta  ke print kore dici*/
+
+
+
+// const numbers = [ 1,2,3,4,5,6,7,8,9,10]
+// const size =numbers.length;
+// console.log(size)
+
+
+
+
+
+
+
+
+
+
+/*  array ar  maje kon index a  kon value  ace ta janer jonno 
+ index number bole dile  index ar majer value ta   easily ber kora jay 
+ tar rules ta dewya holo 
+*/
+// const numbers = [1,2,3,4,5,6,7,8,9]
+ 
+// console.log(numbers[1])
+
+
+// jokhon array ar maje upadan thake na  but amt taw jode  
+//  khoje ber korte chi tobe  output    undefined asbe  
+        
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+console.log(numbers[1])
+
