38d846251ac5072efa74fee64e016cdd65ac155c
Shofikul2024 <islamshofi942@gmail.com>
2025-03-03 07:17:11 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  85 page

--- page-92.js
+++ page-92.js
@@ -13,8 +13,13 @@
 
 
 
-/* akhane arrey ar koita  upadan ace ba array ar lengh koto  ta janar jonno 
-  avave solution orte hobe  */
 
-const numbers = [ 1,2,3,4,5,6,7,8,9,10]
- console.log(numbers.length)
+
+
+/*  array ar  maje kon index a  kon value  ace ta janer jonno 
+ index number bole dile  index ar majer value ta   easily ber kora jay 
+ tar rules ta dewya holo 
+*/
+const numbers = [1,2,3,4,5,6,7,8,9]
+ 
+console.log(numbers[0])
