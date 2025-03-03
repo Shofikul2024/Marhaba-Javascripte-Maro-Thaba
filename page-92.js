92088e1c39b743ba22216b11802a33fcdd270317
Shofikul2024 <islamshofi942@gmail.com>
2025-03-03 07:09:00 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  85 page

--- page-92.js
+++ page-92.js
@@ -1,2 +1,20 @@
+/*  akhane arrey ar koita  upadan ace oita ke  akta size namok variable 
+ ar maje rekhe diye  oi size ta  ke print kore dici*/
+
+
+
+// const numbers = [ 1,2,3,4,5,6,7,8,9,10]
+// const size =numbers.length;
+// console.log(size)
+
+
+
+
+
+
+
+/* akhane arrey ar koita  upadan ace ba array ar lengh koto  ta janar jonno 
+  avave solution orte hobe  */
+
 const numbers = [ 1,2,3,4,5,6,7,8,9,10]
-console.log(numbers.length)
+ console.log(numbers.length)
