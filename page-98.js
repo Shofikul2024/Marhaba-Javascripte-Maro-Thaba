28b44813aaf28d0a1b89dabd16b863360b86fc0d
Shofikul2024 <islamshofi942@gmail.com>
2025-03-05 04:54:44 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-98.js
+++ page-98.js
@@ -11,6 +11,15 @@
 
 
 
+// const friends =[  'balam ' ,  ' kalam '  ,  'salam' ,  'gelam ' ,   'pailam ']
+// friends.unshift('shofikul')
+// console.log(friends)
+
+
+
+//   new array te   element   surute  add korar jonno   unshift korar jonno shift  use 
+//  kora hoy
+
 const friends =[  'balam ' ,  ' kalam '  ,  'salam' ,  'gelam ' ,   'pailam ']
 friends.unshift('shofikul')
 console.log(friends)
