56c2bddd18b2f3f52242e37d7c57f56a61cb43c1
Shofikul2024 <islamshofi942@gmail.com>
2025-03-06 04:46:44 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-97.js
+++ page-97.js
@@ -30,6 +30,11 @@
 
 
 
+// const friends =[ 'balam ' ,' kalam ' , 'salam ' , 'gelam ']
+// const pop1 = friends .pop ()
+// console.log(pop1)
+
+
 const friends =[ 'balam ' ,' kalam ' , 'salam ' , 'gelam ']
 const pop1 = friends .pop ()
 console.log(pop1)
