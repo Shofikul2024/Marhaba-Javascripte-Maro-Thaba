05410135cb7ef33f28acdc291318ea2817ba98c7
Shofikul2024 <islamshofi942@gmail.com>
2025-03-06 15:00:38 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-100.js
+++ page-100.js
@@ -1,6 +1,25 @@
+//  very important rules   space tjakle  i includes  kaj korbe na  
+//   khob valo vabe    be carefuil   vabe kaj korte hyobe  karon  space  thakle   
+//   output dekhabe na     ai  typer  kaj golir khetre    valo  vabe  kaj korte hobe  
+
+
+const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
+ const exist  =friends.includes('balam') ;
+console.log(exist)
+
+
+// const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+// const exist = friends.includes('kalam');
+// console.log(exist);
+
+
+
+
+
+
+
+
 // friends =[  'balam ' ,  ' kalam '  ,  'salam' ,  'gelam ' ,   'pailam ']
 //  const exits =friends.includes('gelam')
 // console.log(exits)
-friends =[  'balam ' ,  ' kalam '  ,  'salam' ,  'gelam ' ,   'pailam ']
- const exits =friends.includes('gelam')
-console.log(exits)
+
