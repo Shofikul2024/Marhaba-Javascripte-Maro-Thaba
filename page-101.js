b49d5eb002dcedc57cff8b89ed9f11efa1a352b6
Shofikul2024 <islamshofi942@gmail.com>
2025-03-06 15:15:55 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-101.js
+++ page-101.js
@@ -1,6 +1,28 @@
+// //  jehutu akhane khilam nai arrey tye tei  else ar maje    jeite ace oita  output  a dekhaben  
+
+
+// const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
+// if (friends.includes('khilam'))  {
+
+//     console.log("party")
+// }
+// else 
+// { 
+
+//     console.log(" no food ewe are  fasting ")
+// }
+
+
+
+
+
+
+/*jehutu akhane kalam  nam, ti  arrey 
+  ar maje ace tai if ar maje  jeita ace  oita  output  a dekhaben    
+*/
 
 const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
-if (friends.includes('khilam'))  {
+if (friends.includes('kalam'))  {
 
     console.log("party")
 }
