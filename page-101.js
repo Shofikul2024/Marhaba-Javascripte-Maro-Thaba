c6ea59fd0dd2f33e3ce4802fa2bcff5a5a586bab
Shofikul2024 <islamshofi942@gmail.com>
2025-03-06 15:12:17 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

---
+++ page-101.js
@@ -0,0 +1,12 @@
+
+const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
+if (friends.includes('khilam'))  {
+
+    console.log("party")
+}
+else 
+{ 
+
+    console.log(" no food ewe are  fasting ")
+}
+
