155875a26013bbfa178c299892644a7ab745eb5c
Shofikul2024 <islamshofi942@gmail.com>
2025-03-06 15:28:59 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-101.js
+++ page-101.js
@@ -21,14 +21,43 @@
   ar maje ace tai if ar maje  jeita ace  oita  output  a dekhaben    
 */
 
+// const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
+// if (friends.includes('kalam'))  {
+
+//     console.log("party")
+// }
+// else 
+// { 
+
+//     console.log(" no food ewe are  fasting ")
+// }
+
+
+
+
+
+//  gelam arrey te ace  but  capital letter ar   Gelam nam    
+//   array te nai tai output a   false  ans asbe karon  Gelam arrey  te nai    
+
+// const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
+//  const exist  =friends.includes('Gelam') ;
+// console.log(exist)
+
+ 
+
+
+//  jehetu arrey  ar maje Gelam nai tai index -1 diyece 
+//  jode array ar maje upadan thake tobe  0 thke  boro index   dekhaben 
+
+// const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
+//  const index  =friends.indexOf('Gelam') ;
+// console.log(index)
+
+
+
+/* akhane  output dekhaben 3 karon 3  nomber index a   gelam ace    */
 const friends = [  'balam' ,  'kalam'  ,  'salam' ,  'gelam' ,   'pailam'] ;
-if (friends.includes('kalam'))  {
+ const index  =friends.indexOf('gelam') ;
+console.log(index)
 
-    console.log("party")
-}
-else 
-{ 
-
-    console.log(" no food ewe are  fasting ")
-}
-
+ 
