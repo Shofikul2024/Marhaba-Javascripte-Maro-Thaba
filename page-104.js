126281e068c0314cd7bb813a85a52b5f528a50cb
Shofikul2024 <islamshofi942@gmail.com>
2025-03-07 06:01:43 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-104.js
+++ page-104.js
@@ -1,5 +1,31 @@
-const  numbers = [ 12,98,45,63,21,72];
-for (const num of numbers ){
+// const  numbers = [ 12,98,45,63,21,72];
+// for (const num of numbers ){
 
-    console.log(num)
-}
+//     console.log(num)
+// }
+
+
+
+
+
+//   array theke   sob goli  element ke pawyer jonno 
+// console.log(numbers)  dite hobe 
+
+
+// const  numbers = [ 12,98,45,63,21,72];
+
+
+// console.log(numbers)
+
+
+
+
+
+//   array theke   sob goli  element ke pawyer jonno 
+// console.log(numbers)  dite hobe 
+
+
+const  numbers = [ 12,98,45,63,21,72];
+
+
+console.log(numbers)
