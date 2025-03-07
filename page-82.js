83ffb29450c5837c485859d7211b74f56890cae5
Shofikul2024 <islamshofi942@gmail.com>
2025-03-08 04:51:40 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-82.js
+++ page-82.js
@@ -15,6 +15,27 @@
 // }
 // else {
 //     console.log(price)
+// // }
+// const price =4000 ;
+// if (price >= 5000){
+
+//     const discount=price/100*10 ;
+//     const pay = price-discount ;
+//     console.log(pay)
+
+// }
+// else if (price>=2500 ) {
+
+//     const discount =price/ 100*5; 
+//     const pay =price-discount ;
+//     console.log( pay)
+
+// }
+// else {
+//     console.log(price)
+// }
+
+
 // }
 const price =4000 ;
 if (price >= 5000){
