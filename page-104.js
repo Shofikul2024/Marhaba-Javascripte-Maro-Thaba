1c33b9e4f1aa9d43e424f43a32e56beb3f10c033
Shofikul2024 <islamshofi942@gmail.com>
2025-03-07 06:02:56 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-104.js
+++ page-104.js
@@ -21,11 +21,12 @@
 
 
 
-//   array theke   sob goli  element ke pawyer jonno 
-// console.log(numbers)  dite hobe 
+
+//   array theke   je kono akti  element ke pawyer jonno 
+// console.log(numbers [2])  dite hobe  jei index ar  value dorker oi index a  ke akhanwe bole   dibo 
 
 
 const  numbers = [ 12,98,45,63,21,72];
 
 
-console.log(numbers)
+console.log(numbers[3])
