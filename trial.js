49600caa597745b17d955d7f9845396f358206bb
Shofikul2024 <islamshofi942@gmail.com>
2025-03-07 10:20:12 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- trial.js
+++ trial.js
@@ -3,4 +3,26 @@
  console.log(10!='10')
 
  console.log(10!=='10')
-//  console.log(true===0)
+ console.log(true===0)
+ console.log(false!='')
+ console.log(false!='')
+
+
+ console.log(true==1)
+ console.log(true!==1)
+
+
+
+ /*
+ uporer code and condition  ar  output goli niche  dewya holo 
+
+ true 
+false
+false
+true 
+false
+false
+false
+true
+true
+*/
