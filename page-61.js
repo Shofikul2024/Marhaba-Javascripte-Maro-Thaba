68c948c3902b56968dd30af3d7bbf55e52e6dfdb
Shofikul2024 <islamshofi942@gmail.com>
2025-03-09 05:58:05 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-61.js
+++ page-61.js
@@ -2,3 +2,8 @@ const fatherAge ='50';
 const  sonAge ='20';
 const  totalAge =fatherAge + sonAge;
 console.log(totalAge)
+
+// const fatherAge ='50';
+// const  sonAge ='20';
+// const  totalAge =fatherAge + sonAge;
+// console.log(totalAge)
