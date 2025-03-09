4c78541d3fdfe4d15761538a629c9eb604f15dd7
Shofikul2024 <islamshofi942@gmail.com>
2025-03-09 12:44:15 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-107.js
+++ page-107.js
@@ -13,8 +13,23 @@
 
 
 
-let num =0 ;
-while (num<20){
-    console.log(num)
-    num=num+1
+// let num =0 ;
+// while (num<20){
+//     console.log(num)
+//     num=num+1
+// }
+
+
+
+// L
+
+
+
+
+let number =0 ;
+while (number<100)
+{ 
+
+    console.log(number)
+    number=number+1
 }
