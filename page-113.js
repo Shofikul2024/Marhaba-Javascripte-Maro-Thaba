ef1643921a580f1cb36833791885e7f1daf3a939
Shofikul2024 <islamshofi942@gmail.com>
2025-03-10 06:37:31 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  85 page

--- page-113.js
+++ page-113.js
@@ -5,7 +5,17 @@
 
 
 
-for (let i=0; i<5; i++){
+// for (let i=0; i<5; i++){
+
+//     console.log(i)
+// }
+
+
+
+// 50 to  100 porjonto number print koro \
+
+
+for (let i=50; i<=100; i++){
 
     console.log(i)
 }
