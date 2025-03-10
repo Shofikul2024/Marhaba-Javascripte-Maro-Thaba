a5f03fb6c66191d93a6f61b080d0ba083bf1bc00
Shofikul2024 <islamshofi942@gmail.com>
2025-03-10 06:24:04 +0600

    Marhaba-Javascripte-Maro-Thaba ,added 85 page

--- page-107.js
+++ page-107.js
@@ -79,23 +79,23 @@
 // 1 theke   50 porjonto odd  number  goli print koro 
 
 
-// let number =1 ;
-
-// while(number<=50)
-// {
-//     console.log(number)
-//      number=number+1
-// }
-
-
-// 1 theke   50 porjonto even  number  goli print koro 
-
-
-let number =2 ;
+let number =1 ;
 
 while(number<=50)
 {
     console.log(number)
      number=number+2
 }
+
+
+// 1 theke   50 porjonto even  number  goli print koro 
+
+
+// let number =2 ;
+
+// while(number<=50)
+// {
+//     console.log(number)
+//      number=number+2
+// }
 
