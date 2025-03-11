20d1f9cd507b063b15162ae121498bdfb0d3fbd0
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-11 23:51:20 +0600

    again same code push the github

---
+++ practice.js
@@ -0,0 +1,21 @@
+const cricketer={
+  position: 4, 
+  specially: 'batter', 
+  age : 24, 
+  
+  runs: 8000,
+  } ;
+console.log(cricketer.position);
+console.log(cricketer['runs']);
+  cricketer.position=1 ;
+cricketer['run']=9000 ;
+
+console.log(cricketer['position']);
+ console.log(cricketer.runs);
+
+
+
+
+
+
+//ai file ta agei upload. kora. hoyecilo. akhon aber upload dilam trial korar jonno
