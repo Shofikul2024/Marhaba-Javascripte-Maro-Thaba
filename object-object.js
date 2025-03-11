6ba73a6584090a0e050c6e28444168b078932a6e
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-12 05:04:15 +0600

    practice object added

---
+++ object-object.js
@@ -0,0 +1,13 @@
+const person ={
+  name :'sodor uddin ',
+  age: 25, 
+  profession : 'developer', 
+  salary:25000, 
+  married:true 
+} ;
+console.log(person.profession) ;
+
+// adding. object and practice using mobile phone  
+
+
+//object is a ipoprtant  topic 
