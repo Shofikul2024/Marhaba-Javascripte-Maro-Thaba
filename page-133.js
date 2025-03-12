4141a05d29cebd125daffd5b7e5a7dda83a2625f
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-12 14:55:58 +0600

    update some code same code push only practici for this. code

--- page-133.js
+++ page-133.js
@@ -9,3 +9,18 @@ const computer= {
  console.log(keys) ;
   const values=Object.values(computer) ;
   console.log(values)
+
+
+
+// added new code trial practice 
+
+const computer= { 
+   brand :'lenovo', 
+ price :36000, 
+ processor: 'intel', 
+ ssd:'512gb'
+ } ;
+ const keys =Object.keys(computer) ;
+ 
+ console.log(keys) ;
+  
