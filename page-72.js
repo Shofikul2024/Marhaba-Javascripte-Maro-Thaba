8e21961a6749132cc7e3fbcc7ec60c7b1423e59b
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-12 15:12:10 +0600

    Update page-72 practice

--- page-72.js
+++ page-72.js
@@ -3,3 +3,16 @@
 // console.log(120 ==="tomato")
 // console.log(120 ==="tomato")
 console.log(120 =="120")
+
+
+/*
+
+
+/ console.log(10 =="10")
+// console.log(10 ==="10")
+// console.log(120 ==="tomato")
+// console.log(120 ==="tomato")
+console.log(120 =="120")
+
+
+  */
