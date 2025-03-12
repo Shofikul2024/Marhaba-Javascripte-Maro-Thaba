e9df526273da7a8d21278b0ab8e89497d7a75687
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-12 14:31:40 +0600

    values topic    added  page-133

---
+++ page-133.js
@@ -0,0 +1,11 @@
+const computer= { 
+   brand :'lenovo', 
+ price :36000, 
+ processor: 'intel', 
+ ssd:'512gb'
+ } ;
+ const keys =Object.keys(computer) ;
+ 
+ console.log(keys) ;
+  const values=Object.values(computer) ;
+  console.log(values)
