f07d3ec0cab9f2825cb77d81f9406107e483ca6b
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-13 04:50:26 +0600

    Update page-133.js

--- page-133.js
+++ page-133.js
@@ -43,3 +43,16 @@ console.log(hasName)
 
 
 */
+
+
+/*
+
+const profile={
+name :'rahim ', 
+age:'28', 
+city:'Dhaka '
+};
+const profileKeys=Object.keys(profile) ;
+const hasName =profileKeys.includes('name')
+console.log(hasName)
+*/
