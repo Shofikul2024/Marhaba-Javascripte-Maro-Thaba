a7d0590c92da4fdbdcf0f4b585583130bd9c3dc6
Shofikul2024 <islamshofi942@gmail.com>
2025-03-14 00:10:26 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  132 page

---
+++ about-git-pull.js
@@ -0,0 +1,10 @@
+/*    git pull command  ar  maddome   mobile diye code kora  
+por  oita  nijr desktop a  ante hole    git pull     command  dite 
+hoy 
+  
+
+ it is the    very  importat  command 
+*/ 
+
+
+git add .
