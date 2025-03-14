8f79494fb392bd39d0c181144bdff0d3f7747a81
Shofikul2024 <islamshofi942@gmail.com>
2025-03-14 06:57:28 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  132 page

---
+++ page-136.js
@@ -0,0 +1,32 @@
+const profile = {
+
+    name :"rahim" ,
+    age: 28 ,
+ city :"dhaka" 
+
+} ;
+
+
+for (const key in profile){
+
+const value= profile[key] ;
+console.log(key,value);
+ 
+
+}
+
+
+/*
+
+
+ jokhon 2 nd  bar  code  push n korbo  tokhon   ai 3 ta  command dite  hobe 
+
+
+
+
+ git add .
+
+
+ 
+
+*/
