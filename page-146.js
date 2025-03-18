ccaec1ae94e115f4c6527478c75f35d2aca256d8
Shofikul2024 <islamshofi942@gmail.com>
2025-03-18 23:53:24 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  132 page

--- page-146.js
+++ page-146.js
@@ -1,3 +1,15 @@
+// function square(number) {
+// const borgo=number*number 
+// console.log('square of' ,number , 'is' ,borgo) 
+
+// }
+// square(4) ;
+// square(12) ;
+
+
+
+//  practice code 
+
 function square(number) {
 const borgo=number*number 
 console.log('square of' ,number , 'is' ,borgo) 
