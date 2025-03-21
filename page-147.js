1cd01b9ecdb357b55ff314ddf00f19c8d314c470
Shofikul2024 <islamshofi942@gmail.com>
2025-03-21 16:05:16 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  132 page

--- page-147.js
+++ page-147.js
@@ -1,5 +1,12 @@
-function add ( num1 ,num2){
-    const sum = num1+num2 ;
-    console.log(sum) ;
+// function add ( num1 ,num2){
+//     const sum = num1+num2 ;
+//     console.log(sum) ;
+// }
+// add( 5,71) ;
+
+
+function add (num1,num2){
+    const sum = num1+num2
+    console.log(sum)
 }
-add( 5,71) ;
+add(5,75)
