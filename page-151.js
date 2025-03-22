1afbc002cb47f58d50fa804fd718fc380e933db9
Shofikul2024 <islamshofi942@gmail.com>
2025-03-22 23:58:10 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  132 page

--- page-151.js
+++ page-151.js
@@ -1,8 +1,22 @@
+// function tenTimes(number){
+
+//     const result= number *10 ;
+//     return result ;
+
+// }
+//  const output=tenTimes(5);
+
+
+
+//  console.log(output)
+
+
+
 function tenTimes(number){
 
     const result= number *10 ;
     return result ;
 
 }
- const output=tenTimes(5);
+ const output=tenTimes(10);
  console.log(output)
