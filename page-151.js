57897deae33cb2ef021d66bcdff46e6a1950eb25
Shofikul2024 <islamshofi942@gmail.com>
2025-03-24 00:41:07 +0600

    project initialized

--- page-151.js
+++ page-151.js
@@ -81,3 +81,17 @@ function tenTimes(number){
 
 
  console.log(output)
+
+
+//   correction this code  
+// function tenTimes(number){
+
+//     const result= number *10 ;
+//     return result ;
+
+// }
+//  const output=tenTimes(5);
+
+
+
+//  console.log(output)
