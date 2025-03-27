5e34b56ead98c34b99e434a53bcfc1e7f53a77e7
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-03-27 10:43:59 +0600

    Update page-107 practice

--- page-107.js
+++ page-107.js
@@ -109,7 +109,24 @@
 
 let number =2 ;
 
-while(number<=50)
+while(number<=50) 
+{
+    console.log(number)
+     number=number+2
+}
+
+
+
+
+
+
+
+// 1 theke   50 porjonto even  number  goli print koro 
+
+
+let number =2 ;
+
+while(number<=50) 
 {
     console.log(number)
      number=number+2
