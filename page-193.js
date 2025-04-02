50ec51eec5294ec6002e6303bbc633de6c4bce42
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-04-02 15:42:52 +0600

    Create page-193.js

---
+++ page-193.js
@@ -0,0 +1,10 @@
+const  student  = {
+name: ' ananta', 
+age: 23 
+}
+const getName= person =>person.name ;
+
+const getAge=(person)  =>person.age ;
+
+const age=getAge(student) ;
+console.log(age) ;
