f4d72efc6c12f5647a5186ccb0138d7e09befdb7
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-04-04 21:43:41 +0600

    Update practice

--- practice.js
+++ practice.js
@@ -247,3 +247,13 @@ const getAge=(person)  =>person.age ;
 const age=getAge(student) ;
 console.log(age) ;
 
+
+
+
+
+
+const add4 =  (num1,  num2, num3, num4) =>num1+num2+num3+num4; 
+
+const sum = add4 (5, 30, 4,6)
+console.log(sum);
+
