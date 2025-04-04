3789c69d8e2c0ee5013200e892c2e1753ff39a0b
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-04-04 22:10:25 +0600

    Update practice.js

--- practice.js
+++ practice.js
@@ -257,3 +257,8 @@ const add4 =  (num1,  num2, num3, num4) =>num1+num2+num3+num4;
 const sum = add4 (5, 30, 4,6)
 console.log(sum);
 
+
+const add4 =  (num1,  num2, num3, num4) =>num1+num2+num3+num4; 
+
+const sum = add4 (5, 30, 4,6)
+console.log(sum);
