17061caf674bf5b44420b9a00f1b2df67f434ce8
Shofikul2024 <islamshofi942@gmail.com>
2025-04-07 16:25:21 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  132 page

---
+++ page-197.js
@@ -0,0 +1,15 @@
+const actor = {
+
+
+    name :'ananata' ,  
+    age : 30  ,  
+    phone :  01746399687,  
+    money:  123654789 ,  
+
+}
+
+const name1 = actor.name ;
+console.log(name1)
+
+// const name = actor.name ;
+// console.log(name)
