b85824416fe622b7cad2690ae843ee85a044a939
Shofikul2024 <islamshofi942@gmail.com>
2025-04-07 18:12:47 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  197  page

--- page-197.js
+++ page-197.js
@@ -1,3 +1,50 @@
+// const actor = {
+
+//     name :'ananata' ,  
+//     age : 30  ,  
+//     phone :  '01746399687',  
+//     money:  123654789 ,  
+
+// };
+
+// const name = actor.name ;
+// console.log(name)
+
+// const age = actor.age ;
+// console.log(age)
+
+
+// const phone = actor.phone ;
+// console.log(phone)
+// const money  = actor.money ;
+// console.log(money)
+
+
+
+
+/*
+
+const actor = {
+
+    name :'ananata' ,  
+    age : 30  ,  
+    phone :  '01746399687',  
+    money:  123654789 ,      
+};
+
+const name = actor.name ;
+const age = actor.age ;
+const phone = actor.phone ;
+const money  = actor.money ;
+ 
+
+const { name , age ,  money  , phone , } =actor 
+console.log( name , age , money , phone )
+
+*/
+
+
+
 const actor = {
 
     name :'ananata' ,  
@@ -8,13 +55,7 @@ const actor = {
 };
 
 const name = actor.name ;
-console.log(name)
-
 const age = actor.age ;
-console.log(age)
-
-
 const phone = actor.phone ;
-console.log(phone)
 const money  = actor.money ;
-console.log(money)
+ console.log( name ,  age , phone , money)
