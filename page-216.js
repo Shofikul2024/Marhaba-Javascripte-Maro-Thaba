fadd4978beeec9cb365880d9aaed5a8724698819
Shofikul2024 <islamshofi942@gmail.com>
2025-04-08 23:15:00 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   216 v page

--- page-216.js
+++ page-216.js
@@ -1,3 +1,11 @@
-const numbers=[4,5,2,8,10 ] ;
-const result =numbers.map(num =>num*2 )
+// const numbers=[4,5,2,8,10 ] ;
+// const result =numbers.map(num =>num*2 )
+// console.log(result)
+
+
+
+//  practice  and practice
+
+const numbers  =[ 4,5,2,8,10]
+const result =numbers.map(num=>num*2)
 console.log(result)
