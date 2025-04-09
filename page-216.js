2907a0bc510926e2414e63e7237bb5afa45911fc
Shofikul2024 <islamshofi942@gmail.com>
2025-04-09 20:10:59 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   217   page

--- page-216.js
+++ page-216.js
@@ -14,10 +14,20 @@
 
 
 
+// //   map related    problem    ,   map khobi pawerful  , jeita  diye  khob   sohojei onk powerful kaj korte  pari 
+
+// const numbers  =[ 4,5,2,8,10]
+// const result =numbers.map(num=>num*2)
+// console.log(result)
+
+
+
+
+
+
 //   map related    problem    ,   map khobi pawerful  , jeita  diye  khob   sohojei onk powerful kaj korte  pari 
 
 const numbers  =[ 4,5,2,8,10]
 const result =numbers.map(num=>num*2)
 console.log(result)
-
 
--- page-217.js
+++ page-217.js
@@ -12,10 +12,10 @@
 
 
 
-//  multiple number add 
-const numbers =[ 1,5,6,4,15 , 16 , 17, 18 ,19]
-const result =numbers.forEach(n=>console.log(n))
-console.log(result)
+// //  multiple number add 
+// const numbers =[ 1,5,6,4,15 , 16 , 17, 18 ,19]
+// const result =numbers.forEach(n=>console.log(n))
+// console.log(result)
 
 
 // const numbers =[ 1,5,6,4,15]
