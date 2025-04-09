d58c15abeef17cc3cf2a9061646492523fc31224
Shofikul2024 <islamshofi942@gmail.com>
2025-04-09 20:09:18 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   217   page

--- page-217.js
+++ page-217.js
@@ -16,3 +16,23 @@
 const numbers =[ 1,5,6,4,15 , 16 , 17, 18 ,19]
 const result =numbers.forEach(n=>console.log(n))
 console.log(result)
+
+
+// const numbers =[ 1,5,6,4,15]
+// const result =numbers.forEach(n=>console.log(n))
+// console.log(result)
+
+//  practice  and  practice 
+
+// const numbers =[ 1,5,6,4,15]
+// const result =numbers.forEach(n=>console.log(n))
+// console.log(result)
+
+
+
+
+
+//  multiple number add 
+const numbers =[ 1,5,6,4,15 , 16 , 17, 18 ,19]
+const result =numbers.forEach(n=>console.log(n))
+console.log(result)
