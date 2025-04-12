805d6aa375b399e1e2711a71ecaee61bf0c13607
Shofikul2024 <islamshofi942@gmail.com>
2025-04-12 12:55:26 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-pc-132.js
+++ page-pc-132.js
@@ -31,6 +31,22 @@
 
 
   
+// const computer= { 
+//     brand :'lenovo', 
+//   price :36000, 
+//   processor: 'intel', 
+//   ssd:'512gb'
+//   } ;
+//   const keys =Object.keys(computer) ;
+  
+//   console.log(keys) ;
+  
+  
+
+
+
+
+//   same    code   
 const computer= { 
     brand :'lenovo', 
   price :36000, 
