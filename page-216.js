801893d11e6d822f3e9e0a237f403469dae2530a
Shofikul2024 <islamshofi942@gmail.com>
2025-04-16 21:36:57 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-216.js
+++ page-216.js
@@ -31,3 +31,10 @@ const numbers  =[ 4,5,2,8,10]
 const result =numbers.map(num=>num*2)
 console.log(result)
 
+
+//   map related    problem    ,   map khobi pawerful  , jeita  diye  khob   sohojei onk powerful kaj korte  pari 
+
+const numbers  =[ 4,5,2,8,10]
+const result =numbers.map(num=>num*2)
+console.log(result)
+
