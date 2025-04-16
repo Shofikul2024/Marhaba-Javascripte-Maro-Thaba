3b3cf3e2d925a5d3cb0190cf1b0eaf5d5ee4e8b1
Shofikul2024 <islamshofi942@gmail.com>
2025-04-16 21:37:27 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-164.js
+++ page-164.js
@@ -5,4 +5,18 @@ function inchToFeet(inch) {
 const shovoHeight = inchToFeet(75);
 console.log(shovoHeight)
 
+
+
+
 // akdom correct code 
+// function inchToFeet(inch) {
+//   const feet = inch/12 ;
+//   return  feet ;
+// }
+// const shovoHeight = inchToFeet(75);
+// console.log(shovoHeight)
+
+// // akdom correct code 
+
+
+
--- page-216.js
+++ page-216.js
@@ -32,9 +32,9 @@ const result =numbers.map(num=>num*2)
 console.log(result)
 
 
-//   map related    problem    ,   map khobi pawerful  , jeita  diye  khob   sohojei onk powerful kaj korte  pari 
+// //   map related    problem    ,   map khobi pawerful  , jeita  diye  khob   sohojei onk powerful kaj korte  pari 
 
-const numbers  =[ 4,5,2,8,10]
-const result =numbers.map(num=>num*2)
-console.log(result)
+// const numbers  =[ 4,5,2,8,10]
+// const result =numbers.map(num=>num*2)
+// console.log(result)
 
