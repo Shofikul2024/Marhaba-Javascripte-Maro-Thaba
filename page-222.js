d36e760c4ee4b2bf824824bfbf076b465465b9ba
Shofikul2024 <islamshofi942@gmail.com>
2025-04-19 15:00:29 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-222.js
+++ page-222.js
@@ -26,24 +26,31 @@ console.log(hasPassed) */
 
 
 // some method   practice  .  trial and practice 
+// const scores =[ 30,45,55,80,90]
+// const hasPassed =scores.some(score=> score >=50 ) ;
+// console.log(hasPassed) 
+
+
+
+
+
+// // some method   practice  .  trial and practice 
+// const scores =[ 30,45,55,80,90]
+// const hasPassed =scores.some(score=> score >=50 ) ;
+// console.log(hasPassed) 
+
+
+
+// // some method   practice  .  trial and practice 
+// const scores =[ 30,45,55,80,90]
+// const hasPassed =scores.some(score=> score >=50 ) ;
+// console.log(hasPassed) 
+
+
+
+// some method   practice  .  trial and practice 
 const scores =[ 30,45,55,80,90]
 const hasPassed =scores.some(score=> score >=50 ) ;
 console.log(hasPassed) 
-
-
-
-
-
-// // some method   practice  .  trial and practice 
-// const scores =[ 30,45,55,80,90]
-// const hasPassed =scores.some(score=> score >=50 ) ;
-// console.log(hasPassed) 
-
-
-
-// // some method   practice  .  trial and practice 
-// const scores =[ 30,45,55,80,90]
-// const hasPassed =scores.some(score=> score >=50 ) ;
-// console.log(hasPassed) 
 
 
