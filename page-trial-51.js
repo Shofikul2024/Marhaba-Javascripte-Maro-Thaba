4ca0a01308a9aa43069a1e9eca6b85a0f32dd0f5
Shofikul2024 <islamshofi942@gmail.com>
2025-04-22 14:02:47 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-trial-51.js
+++ page-trial-51.js
@@ -1,9 +1,15 @@
-let price = 35 ;
-price= 45 ;
-console.log(price)
+// let price = 35 ;
+// price= 45 ;
+// console.log(price)
 
 
 //   practice  and practice 
 // let price = 35 ;
 // price= 45 ;
 // console.log(price)
+
+
+//    again and again   practice  and practice 
+let price = 35 ;
+price= 45 ;
+console.log(price)
