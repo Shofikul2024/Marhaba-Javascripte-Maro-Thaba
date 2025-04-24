34ba6b59da678d60d710753db62513722f78d66e
Shofikul2024 <islamshofi942@gmail.com>
2025-04-24 17:40:37 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- practice.js
+++ practice.js
@@ -430,6 +430,29 @@ const  student  = {
   // console.log(age) ;
 
 
+
+
+
+
+
+  //  21  a april  aber n practice  
+
+  // const  student  = {
+  // name: ' ananta', 
+  // age: 23 
+  // }
+  // const getName= person =>person.name ;
+  
+  // const getAge=(person)  =>person.age ;
+  
+  // const age=getAge(student) ;
+  // console.log(age) ;
+
+
+  
+
+  
+
    21  a april  aber n practice  
 
   const  student  = {
