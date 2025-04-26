c365ed1819de8739a329471c232e6e08db16e28f
Shofikul2024 <islamshofi942@gmail.com>
2025-04-26 15:57:41 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-218.js
+++ page-218.js
@@ -7,3 +7,12 @@ console.log(selected)
 // const players=[75,65,67,71,55,59]
 // const selected =players.filter(p=>p>70 ) ;
 // console.log(selected)
+// const players=[75,65,67,71,55,59]
+// const selected =players.filter(p=>p>70 ) ;
+// console.log(selected)
+
+
+
+// // const players=[75,65,67,71,55,59]
+// // const selected =players.filter(p=>p>70 ) ;
+// // console.log(selected)
