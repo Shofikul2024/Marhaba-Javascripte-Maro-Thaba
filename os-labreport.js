874d0579f794e15f31db4b57dad23ae8e8e79216
Shofikul2024 <islamshofi942@gmail.com>
2025-04-28 21:22:04 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

---
+++ os-labreport.js
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+echo "Enter 1st number:"
+read a
+
+echo "Enter 2nd number:"
+read b
+
+sum=$(($a + $b))
+sub=$(($a - $b))
+product=$(($a * $b))
+div=$(($a / $b))
+
+echo "Sum = $sum"
+echo "Subtraction = $sub"
+echo "Product = $product"
+echo "Division = $div"
+
+
+
+
+mkdir  foldername
+vim foldername
+click i
+ESC
+shift:wq
+bash samefolder name
+
