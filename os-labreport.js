7defe922db875769c144eb72fd295995ac46d4b7
Shofikul2024 <islamshofi942@gmail.com>
2025-04-28 21:22:26 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- os-labreport.js
+++ os-labreport.js
@@ -1,29 +1,61 @@
 
-#!/bin/bash
+// #!/bin/bash
 
-echo "Enter 1st number:"
-read a
+// echo "Enter 1st number:"
+// read a
 
-echo "Enter 2nd number:"
-read b
+// echo "Enter 2nd number:"
+// read b
 
-sum=$(($a + $b))
-sub=$(($a - $b))
-product=$(($a * $b))
-div=$(($a / $b))
+// sum=$(($a + $b))
+// sub=$(($a - $b))
+// product=$(($a * $b))
+// div=$(($a / $b))
 
-echo "Sum = $sum"
-echo "Subtraction = $sub"
-echo "Product = $product"
-echo "Division = $div"
-
-
+// echo "Sum = $sum"
+// echo "Subtraction = $sub"
+// echo "Product = $product"
+// echo "Division = $div"
 
 
-mkdir  foldername
-vim foldername
-click i
-ESC
-shift:wq
-bash samefolder name
+
+
+// mkdir  foldername
+// vim foldername
+// click i
+// ESC
+// shift:wq
+// bash samefolder name
+
+
+
+//   practice  
+
+// #!/bin/bash
+
+// echo "Enter 1st number:"
+// read a
+
+// echo "Enter 2nd number:"
+// read b
+
+// sum=$(($a + $b))
+// sub=$(($a - $b))
+// product=$(($a * $b))
+// div=$(($a / $b))
+
+// echo "Sum = $sum"
+// echo "Subtraction = $sub"
+// echo "Product = $product"
+// echo "Division = $div"
+
+
+
+
+// mkdir  foldername
+// vim foldername
+// click i
+// ESC
+// shift:wq
+// bash samefolder name
 
