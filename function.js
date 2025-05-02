ab4971635d9394ff6a8c0ef91f2a293a1321e971
Shofikul2024 <islamshofi942@gmail.com>
2025-05-02 23:29:43 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- function.js
+++ function.js
@@ -1,8 +1,39 @@
-function multiply (num1 , num2) {
+// function multiply (num1 , num2) {
 
-return num1*num2
+// return num1*num2
+
+// }
+// result = multiply( 5 ,6)
+
+// console.log(result)
+
+
+
+
+// practice  
+
+
+// function multiply (num1 , num2) {
+// const result= num1*num2 ;
+// return num1*num2
+
+// }
+// result = multiply( 10 ,6)
+
+// console.log(result)
+
+
+
+
+
+// practice 
+
+
+function multiply (num1 , num2) {
+const result= num1*num2 ;
+return result
 
 }
-result = multiply( 5 ,6)
+result = multiply( 20,6)
 
 console.log(result)
