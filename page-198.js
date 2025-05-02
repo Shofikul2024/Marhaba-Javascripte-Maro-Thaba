87c24bd517cd6b4cedcf3e2e813e0a525776ee56
Shofikul2024 <islamshofi942@gmail.com>
2025-05-02 11:17:03 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-198.js
+++ page-198.js
@@ -1,5 +1,25 @@
 // page  197    desturucturing    mane  holo  array  ba object ke   choto choto  kore  vak kore  fela  . 
 
+// const actor = {
+
+
+//      name : 'ananta' ,
+//      age : 30 ,
+//      phone: '017456812' ,
+//      money : 12457814714 ,
+// }
+
+
+
+// const name =actor.name  ;
+// const age =actor.age  ;
+// const phone =actor.phone  ;
+// const money=actor.money  ;
+// console.log(name ,age , phone , money ) 
+
+
+
+
 const actor = {
 
 
@@ -9,10 +29,8 @@ const actor = {
      money : 12457814714 ,
 }
 
-
-
-const name =actor.name  ;
-const age =actor.age  ;
-const phone =actor.phone  ;
-const money=actor.money  ;
-console.log(name ,age , phone , money ) 
+const { name , age , phone , money   } =actor
+console.log(name);
+console.log(age);
+console.log(money);
+console.log(phone);
