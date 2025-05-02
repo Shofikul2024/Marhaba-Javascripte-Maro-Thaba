bbc4695a31a5eb5119b30cc7fadb204d57f85f0b
Shofikul2024 <islamshofi942@gmail.com>
2025-05-02 11:25:41 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- page-198.js
+++ page-198.js
@@ -18,19 +18,53 @@
 // console.log(name ,age , phone , money ) 
 
 
+// const actor = {
 
 
-const actor = {
+//      name : 'ananta' ,
+//      age : 30 ,
+//      phone: '017456812' ,
+//      money : 12457814714 ,
+// }
+
+// const { name , age , phone , money   } =actor
+// console.log(name);
+// console.log(age);
+// console.log(money);
+// console.log(phone);
 
 
-     name : 'ananta' ,
-     age : 30 ,
-     phone: '017456812' ,
-     money : 12457814714 ,
+
+// //  console  log  ar somoy  jode  ai vave  koma  diye  likhi  tahole  aki line  a  sob  goli dekhabe . jemon console.log( name , age , money , phone )
+// //shofikul  30 142554665754  017463998687
+
+// name:'shofikul ',
+// age :30 ,
+// phone: ' 017463998687',
+// money:142554665754,
+
+
+// }
+
+
+// const { name ,age , phone , money  }  = actor
+// console.log( name , age , money , phone )
+
+
+
+
+
+//  console  log  ar somoy  jode  ai vave  koma  diye  likhi  tahole  aki line  a  sob  goli dekhabe . jemon console.log( name , age , money , phone )
+//shofikul  30 142554665754  017463998687
+
+name:'shofikul ',
+age :30 ,
+phone: ' 017463998687',
+money:142554665754,
+
+
 }
 
-const { name , age , phone , money   } =actor
-console.log(name);
-console.log(age);
-console.log(money);
-console.log(phone);
+
+const { name ,age , phone , money  }  = actor
+console.log( name , age , money , phone )
