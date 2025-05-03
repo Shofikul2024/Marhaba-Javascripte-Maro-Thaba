817121689a353c49baad27c14167f06c2bf4fc63
Shofikul2024 <islamshofi942@gmail.com>
2025-05-03 11:58:54 +0600

    Marhaba-Javascripte-Maro-Thaba ,added   219   page

--- destructuring-m7-37-4.js
+++ destructuring-m7-37-4.js
@@ -1,6 +1,6 @@
 
 
-// array   destructuring 
+//1    no     array   destructuring 
 
 
 // const numbers =[ 42 ,65] ;
@@ -10,8 +10,19 @@
 
 
 
-//  aita ke  short  vabe  kora  jay aitai array destructuring 
+// 2. no     aita ke  short  vabe  kora  jay aitai array destructuring 
 
 
-const [ x, y] =[ 42 , 65]
+// const [ x, y] =[ 42 , 65]
+// console.log( x,y)
+
+
+
+// //  3 no  given  numbers =[42,65]
+// //orthad   akhane  numbers manei holo [42, 65] 
+
+
+
+const numbers = [42, 65] 
+const [ x, y] =numbers
 console.log( x,y)
