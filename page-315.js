00b2a5efa70d9d29435dab87e700cd58c4abd08c
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-04 20:18:37 +0600

    added update code

--- page-315.js
+++ page-315.js
@@ -11,15 +11,33 @@
 // console.log(userJSON)
 
 
-// note 
-const user ={
-id: 1,
-name: 'amir',
-job: 'actor',
+// // note 
+// const user ={
+// id: 1,
+// name: 'amir',
+// job: 'actor',
 
 
+
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+
+
+
+
+
+const numbers =[1,2,3,4,5,6,7]
+
+let evens=[]
+
+for ( let item  of numbers) {
+if ( item%2===0) {
+
+    evens.push(item)
+}
 
 }
 
-const userJSON = JSON.stringify(user)
-console.log(userJSON)
+console.log(evens)
