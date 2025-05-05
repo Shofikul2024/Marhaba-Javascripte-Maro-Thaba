fd86ea27cc7f03eed07067309c37ae323cdbe01e
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-05 14:50:32 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

--- page-46.js
+++ page-46.js
@@ -1,3 +1,9 @@
-const first = 'Mobarak '
-const second = 'tobarak '
+// const first = 'Mobarak '
+// const second = 'tobarak '
+// console.log(first+second)
+
+// practice 
+
+const first = 'Eid  '
+const second = ' Mobarak '
 console.log(first+second)
