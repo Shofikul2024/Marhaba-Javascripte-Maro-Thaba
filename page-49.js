9f7a444392f0ef11f5cb1b8055f3e115955ef9d5
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-05 15:25:15 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

--- page-49.js
+++ page-49.js
@@ -43,3 +43,11 @@
 // console.log( balance)
 
 
+
+
+
+const first =12.13568 ;
+const second =31.22564 ;
+const total= first+second ;
+const totalNumber=parseFloat(total.toFixed(2))
+console.log(totalNumber)
