dffc92e2b27c4d3273b80c949c6832f0fcc1cfe2
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-06 01:01:30 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

--- cs-es-6-arrow.js
+++ cs-es-6-arrow.js
@@ -1,2 +1,13 @@
-const add =(x,y) => x+y 
-console.log(add(2,5))
+// const add =(x,y) => x+y 
+// console.log(add(2,5))
+
+
+
+
+
+function add (x,y)  {
+    
+    return x+y 
+
+}
+console.log(add(4,6))
