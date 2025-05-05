061e2075ab1576221f10b168be5ddebc87f1fd91
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-05 16:43:18 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

--- page-238.js
+++ page-238.js
@@ -12,3 +12,28 @@
 // const name =  'abid' ;
 // console.log(name[3]) ;
 // output     d
+
+
+
+
+// const numbers=[54,98,78,21,65]
+// numbers[1]=11 ;
+// console.log(numbers)
+
+
+
+
+
+
+const subject ='Chemistry ';
+const book='chemistry';
+if(subject.toLowerCase()===book.toLowerCase())
+{
+    console.log(' i am reading book ');
+
+}
+else {
+
+console.log('Huday prista ultai ');
+
+}
