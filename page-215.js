74ab181943aaf91fcf323cab0f608550c49ad981
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-07 21:16:00 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

--- page-215.js
+++ page-215.js
@@ -26,25 +26,24 @@
 
 
 
+//   very  important   rules aikhane   paremeter  ar   anm  jekono ta   dite  parbo  
+
+//   but   akhane  sorbosesh  a  function  ti  jekhane  call   korbo  oikhane  argument  dite  hobe
+//  argument   hisebe  akhane array   ti ke  dewya  hoyece . 
 
 
-const numbers =[4,5,4,16,20]
-function doubleAll( number) {
+// const numbers =[4,5,4,16,20]
+// function doubleAll( numberItIsParameter) {
 
-const doubled=[] ;
+// const doubled=[] ;
 
-for (const num of numbers){
-   const value=num*2 ;
-   doubled.push(value)   ;
- }
-return doubled ;
-}
-console.log( doubleAll(numbers) )
-
-
-
-
-
+// for (const num of numbers){
+//    const value=num*2 ;
+//    doubled.push(value)   ;
+//  }
+// return doubled ;
+// }
+// console.log( doubleAll(numbers) )
 
 
 
@@ -54,7 +53,24 @@ console.log( doubleAll(numbers) )
 // practice  and practice 
 
 
+// const numbers =[4,5,4,16,20]
+// function doubleAll( numberItIsParameter) {
 
+// const doubled=[] ;
+
+// for (const num of numbers){
+//    const value=num*2 ;
+//    doubled.push(value)   ;
+//  }
+// return doubled ;
+// }
+// console.log( doubleAll(numbers) )
+
+
+
+
+
+// practice  and practice 
 
 // function  doubleAll(numbers){
 
@@ -73,3 +89,23 @@ console.log( doubleAll(numbers) )
 // console.log(doubleAll(numbers)) ;
 
 // output :[ 8, 10, 4, 16, 20 ]
+
+
+
+
+
+
+///   practice 
+const numbers = [ 4,5,2,8,10]
+function trippleedAll() {
+const doubled =[] ;
+
+for (const num of numbers) {
+
+    const value=num*3 ;
+    doubled.push(value)
+}
+return doubled
+}
+ console.log(  trippleedAll(numbers) )
+
