76082e755d5d6e7436234bf5bf95e23e549f69ac
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-07 20:41:58 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

--- page-215.js
+++ page-215.js
@@ -1,17 +1,78 @@
-const numbers=[4,5,2,8,10] ;
+// aita  valo format ar  function   , aivabe kora ta   valo hoy  .
 
-function  doubleAll(numbers){
+// const numbers=[4,5,2,8,10] ;
+
+// function  doubleAll(numbers){
+
+// const doubled=[] ;
+// for (const num of numbers) {
+
+// const value=num*2 ;
+
+// doubled.push(value) ;
+// }
+
+// return doubled ;
+
+// }
+
+// console.log(doubleAll(numbers)) ;
+
+
+// output :[ 8, 10, 4, 16, 20 ]
+
+
+
+
+
+
+
+
+const numbers =[4,5,4,16,20]
+function doubleAll( number) {
 
 const doubled=[] ;
-for (const num of numbers) {
 
-const value=num*2 ;
+for (const num of numbers){
+   const value=num*2 ;
+   doubled.push(value)   ;
 
-doubled.push(value) ;
-
-}
+ }
 return doubled ;
 
 }
 
-console.log(doubleAll(numbers)) ;
+console.log( doubleAll(numbers) )
+
+
+
+
+
+
+
+
+
+
+
+// practice  and practice 
+
+
+
+
+// function  doubleAll(numbers){
+
+// const doubled=[] ;
+// for (const num of numbers) {
+
+// const value=num*2 ;
+
+// doubled.push(value) ;
+// }
+
+// return doubled ;
+
+// }
+// const numbers=[4,5,2,8,10] ;
+// console.log(doubleAll(numbers)) ;
+
+// output :[ 8, 10, 4, 16, 20 ]
