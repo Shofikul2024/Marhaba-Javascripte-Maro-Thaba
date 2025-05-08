213e33124613bd3e67f9ab3aa1f11ae5ef3d8df4
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-05-09 02:40:25 +0600

    Marhaba-Javascripte-Maro-Thaba ,added  practice part   page

---
+++ hello.js/hello2.js
@@ -0,0 +1,14 @@
+// console.log('shofikul')
+
+
+
+const  x= 10 ;
+const  y= 20 ;
+const result=x+y  ;
+console.log(result)
+
+// const  x= 10 ;
+// const  y= 20 ;
+// const result=x+y  ;
+// console.log(result)
+
