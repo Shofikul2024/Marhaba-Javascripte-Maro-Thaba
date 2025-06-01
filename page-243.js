733539bd3466d71c2efc9fcb766c9942aabfcaa1
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-02 00:06:24 +0600

    added  same name but different value

--- page-243.js
+++ page-243.js
@@ -1,4 +1,53 @@
-const drink ="water" ;
+/*
+1 st rules   a  pani hani dami jani ans  a  dekhabee      karon  2 ta valur maje kono difference nai 
+
+*/
+
+
+
+// 1  first     rules 
+// const drink ="water" ;
+// const liquid ="water" ;
+
+
+// if (drink===liquid)
+// {
+
+
+//     console.log("pani hani  dami jani ")
+// }
+
+
+// else
+
+// {
+
+//     console.log("sumeder pani dekte valo khite kalo ")
+
+    
+// }
+
+
+
+
+
+
+
+
+
+// 2  second      rules 
+
+/*
+2nd   rules   a   sumeder pani dekte valo khite kalo  karon  2 ta valur maje  difference   acea   first valur  maje spacee  ace tai   sumeder pani dekte valo khite kal  dekhabe 
+
+*/
+
+
+
+
+
+
+const drink =" water" ;
 const liquid ="water" ;
 
 
@@ -6,7 +55,7 @@ if (drink===liquid)
 {
 
 
-    console.log("pani hani")
+    console.log("pani hani  dami jani ")
 }
 
 
@@ -14,7 +63,8 @@ else
 
 {
 
-    console.log(" somber  pani")
+    console.log("sumeder pani dekte valo khite kalo ")
 
     
 }
+
