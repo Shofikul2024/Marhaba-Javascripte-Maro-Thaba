d2e7ba5e5aeb13caa853e87d44328442a488de14
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-21 00:00:17 +0600

    addded page 256

--- page-256.js
+++ page-256.js
@@ -26,3 +26,31 @@ let q=p ;
 console.log(p,q);
 q.job='front-end developer' ;
 console.log(p,q);
+
+
+
+
+// let p= {
+
+//     job:'web developer'
+
+// };
+
+// let q=p ;
+// console.log(p,q);
+// q.job='front-end developer' ;
+// console.log(p,q);
+
+
+
+
+// let p= {
+
+//     job:'web developer'
+
+// };
+
+// let q=p ;
+// console.log(p,q);
+// q.job='front-end developer' ;
+// console.log(p,q);
