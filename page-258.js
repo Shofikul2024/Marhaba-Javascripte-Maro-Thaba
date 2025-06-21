1d15d4f579c36df8ec4b1acad3223bf6fbcf10ea
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-21 23:56:16 +0600

    addded page 256

---
+++ page-258.js
@@ -0,0 +1,21 @@
+// function changeValue(num){
+
+//     num =20 ;
+//     console.log('Inside function :',num);
+// }
+// let x= 10 ;
+// console.log('Before function call :', x);
+// changeValue(x);
+// console.log('After function call:',x);
+// }
+
+
+function changeValue(num){
+    num = 20;
+    console.log('Inside function :', num);
+}
+
+let x = 10;
+console.log('Before function call :', x);
+changeValue(x);
+console.log('After function call:', x);
