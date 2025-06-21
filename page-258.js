a1145d4024828343f7faa38a0a16d63585c2b50b
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-21 23:59:07 +0600

    addded page 256

--- page-258.js
+++ page-258.js
@@ -1,21 +1,25 @@
-// function changeValue(num){
-
-//     num =20 ;
-//     console.log('Inside function :',num);
-// }
-// let x= 10 ;
-// console.log('Before function call :', x);
-// changeValue(x);
-// console.log('After function call:',x);
-// }
-
+//correction   2nd bracket   vul cilo ,now  oita corrction  korci 
 
 function changeValue(num){
-    num = 20;
-    console.log('Inside function :', num);
-}
 
-let x = 10;
+    num =20 ;
+    console.log('Inside function :',num);
+}
+let x= 10 ;
 console.log('Before function call :', x);
 changeValue(x);
-console.log('After function call:', x);
+console.log('After function call:',x);
+
+
+
+
+
+// function changeValue(num){
+//     num = 20;
+//     console.log('Inside function :', num);
+// }
+
+// let x = 10;
+// console.log('Before function call :', x);
+// changeValue(x);
+// console.log('After function call:', x);
