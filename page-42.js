012ca7619806e811fad08f82b8cb6744ef25db4e
shofikul2024 <islamshofik492@gmail.com>
2025-09-24 15:36:01 +0600

    added  jabascript updated  added three number

--- page-42.js
+++ page-42.js
@@ -17,7 +17,18 @@
 // console.log(pased)
 
 
-const isRich=true ;
-const pased=false;
-console.log(isRich);
-console.log(pased);
+// const isRich=true ;
+// const pased=false;
+// console.log(isRich);
+// console.log(pased);
+
+
+
+x=10
+y=20
+z=30
+result= x + y + z
+console.log(result)
+console.log(x)
+console.log(y)
+console.log(z)
--- page-trial-51.js
+++ page-trial-51.js
@@ -10,10 +10,16 @@
 
 
 //    again and again   practice  and practice 
-let price = 35 ;
-price= 45 ;
-console.log(price)
+// let price = 35 ;
+// price= 45 ;
+// console.log(price)
 // //    again and again   practice  and practice 
 // let price = 35 ;
 // price= 45 ;
 // console.log(price)
+
+
+
+let price = 70;
+price= 50 ;
+console.log(price)
