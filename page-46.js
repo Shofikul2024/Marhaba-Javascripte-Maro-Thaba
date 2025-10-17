12ed1e456a7ac7f18d30b8d54ee4a8a753e3de41
shofikul2024 <islamshofik492@gmail.com>
2025-10-17 14:56:08 +0600

    added   sub

--- page-46.js
+++ page-46.js
@@ -9,6 +9,15 @@
 // console.log(first+second)
 
 
-console.log('shofikul islam')
-console.log('shofikul islam')
-console.log('shofikul islam')
+// console.log('shofikul islam')
+// console.log('shofikul islam')
+// console.log('shofikul islam')
+
+x=10
+y=20
+z=30
+result= x + y + z
+console.log(result)
+console.log(x)
+console.log(y)
+console.log(z)
