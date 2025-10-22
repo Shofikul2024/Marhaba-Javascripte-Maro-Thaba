216da160bc55993f43576cd92a3d69c9eebf5e6e
shofikul2024 <islamshofi942@gmail.com>
2025-10-22 17:33:54 +0600

    added basic summation

--- hello.js/hello2.js
+++ hello.js/hello2.js
@@ -2,13 +2,17 @@
 
 
 
-const  x= 10 ;
-const  y= 20 ;
-const result=x+y  ;
-console.log(result)
+// const  x= 10 ;
+// const  y= 20 ;
+// const result=x+y  ;
+// console.log(result)
 
 // const  x= 10 ;
 // const  y= 20 ;
 // const result=x+y  ;
 // console.log(result)
 
+const x= 10 ;
+const y= 20 ;
+const result=x+y  ;
+console.log(result)
